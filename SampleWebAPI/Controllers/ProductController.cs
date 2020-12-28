7e575361d13338358746e18894b9eadee72bf2a2
ruangb <ruangbarros@gmail.com>
2020-12-28 00:27:13 -0300

    Creating a Sample Web API

---
+++ SampleWebAPI/Controllers/ProductController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SampleWebAPI.Data;
+using SampleWebAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleWebAPI.Controllers
+{
+    [Route("v1/products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Product>>> Get([FromServices] DataContext context)
+        {
+            var products = await context.Products.Include(x => x.Category).ToListAsync();
+            return products;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Product>> GetById([FromServices] DataContext context, int id)
+        {
+            var product = await context.Products.Include(x => x.Category)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            return product;
+        }
+
+        [HttpGet]
+        [Route("categories/{id:int}")]
+        public async Task<ActionResult<List<Product>>> GetByCategory([FromServices] DataContext context, int id)
+        {
+            var products = await context.Products
+                .AsNoTracking()
+                .Include(x => x.Category)
+                .Where(x => x.Id == id).ToListAsync();
+
+            return products;
+        }
+
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Product>> Post(
+            [FromServices] DataContext context,
+            [FromBody] Product model)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Products.Add(model);
+                await context.SaveChangesAsync();
+                return model;
+            }
+            else
+                return BadRequest(ModelState);
+        }
+    }
+}
---
+++ SampleWebAPI/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleWebAPI.Models;
+
+namespace SampleWebAPI.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Product> Products{ get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
